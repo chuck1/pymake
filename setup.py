1652604861d54878da42a6e3921ccc7d803938d8
Charles Rymal <charles.rymal@nortek.com>
2017-05-16 17:04:08 -0700

    auto commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(name='pymake',
+        version='0.1',
+        description='python makefile system',
+        url='http://github.com/chuck1/pymake',
+        author='Charles Rymal',
+        author_email='charlesrymal@gmail.com',
+        license='MIT',
+        packages=['pymake'],
+        zip_safe=False,
+        )
+
