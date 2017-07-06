daca6adc0e1ccc40064c2cef8b3cd18a1642b14b
Charles Rymal <charles.rymal@nortek.com>
2017-07-06 08:59:37 -0700

    fixing tests

--- pymake/tests/test_basic_2/test_1.py
+++ pymake/tests/test_basic_2/test_1.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
 import subprocess
+
 import pymake
 
 def touch(filename):
     subprocess.call(['touch',filename])
 
-m = pymake.Makefile()
+def test():
+    m = pymake.Makefile()
 
-m.rules['all'] = pymake.Rule('build/a', ['build/b'], lambda fout, fin: touch(fout))
-m.rules['build/b'] = pymake.Rule('build/b', [], lambda fout, fin: touch(fout))
-
-m.make('all')
+    m.rules['all'] = pymake.Rule('build/a', ['build/b'], lambda fout, fin: touch(fout))
+    m.rules['build/b'] = pymake.Rule('build/b', [], lambda fout, fin: touch(fout))
+    
+    m.make('all')
 
