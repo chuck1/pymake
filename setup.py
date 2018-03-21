c2f31e15764e0e1c9a73e87216aa58c0575b2551
Charles Rymal <charles.rymal@nortek.com>
2018-03-20 17:15:33 -0700

    auto commit

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 import re
 from setuptools import setup
 
-with open('pymake/__init__.py') as f:
-    version = re.findall("^__version__ = '(.*)'", f.read())[0]
 
-setup(name='pymake',
-        version=version,
-        description='python makefile system',
-        url='http://github.com/chuck1/pymake',
+setup(name='mybuiltins',
+        version='0.1',
+        description='',
+        url='',
         author='Charles Rymal',
         author_email='charlesrymal@gmail.com',
         license='MIT',
         packages=[
-            'pymake',
-            'pymake.tests',
+            'mybuiltins',
             ],
         zip_safe=False,
         )
